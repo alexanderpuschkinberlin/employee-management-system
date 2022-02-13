3a3ddb9e9f9737026c182bc9da9f4ac4f1231316
alexanderpuschkinberlin <90645435+alexanderpuschkinberlin@users.noreply.github.com>
2022-02-13 13:41:00 +0100

    Add skeleton for inquirer

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
 const inquirer = require("inquirer");
 
-const init = async () => {
-  try {
-    const answer = await inquirer.prompt([
-      {
-        type: "input",
-        name: "first_name",
-        message: "What's your first name",
-      },
-    ]);
+const showEmployees = async () => {
+  console.log("Showing all the employees");
+};
 
-    console.log("Answer : ", answer);
-  } catch (err) {
-    console.log("Err ocurred while asking a question", err);
+const init = async () => {
+  let shouldExit = true;
+
+  while (shouldExit) {
+    try {
+      const answer = await inquirer.prompt([
+        {
+          type: "list",
+          name: "choice",
+          message: "What do you want to do?",
+          choices: [
+            { value: 1, name: "Show all the employees" },
+            { value: 2, name: "Show all the roles" },
+            { value: 3, name: "Show all the departments" },
+            { value: 4, name: "Exit" },
+          ],
+        },
+      ]);
+
+      //process the answers
+      switch (answer.choice) {
+        case 1: // show all employees;
+          await showEmployees();
+          break;
+        case 2: // show all roles;
+          break;
+        case 3: // show all departments;
+          break;
+        case 4: // exit
+        default:
+          shouldExit = false;
+      }
+    } catch (err) {
+      console.log("Err ocurred while asking a question", err);
+    }
   }
 };
+
 init();
